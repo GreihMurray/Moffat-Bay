caaecf3835ecc8bd633b02177e01e2d474c94610
Greih Murray <murrayg0216@outlook.com>
2024-10-26 06:15:29 -0500

    Adding base project

---
+++ src/main/java/com/moffat/bay/MoffatBay/MoffatBayApplication.java
@@ -0,0 +1,13 @@
+package com.moffat.bay.MoffatBay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoffatBayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoffatBayApplication.class, args);
+	}
+
+}
