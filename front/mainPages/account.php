75a7123acae49f1702eaeb83dfed42543646eccf
Sam Bachman <94061452+sbachma@users.noreply.github.com>
2024-11-16 18:41:47 -0600

    small changes

--- front/mainPages/account.php
+++ front/mainPages/account.php
@@ -2,47 +2,52 @@
     $title = "Login and Registration Page";
     $root = "../../";
     require_once('../border/header.php');
-    $loggingIn = true;
 ?>
 
 <link rel="stylesheet" type="text/css" href="<?php echo css ?>login.css">
 
 <div class="loginRegistrationForm">
-    <!-- Login Form -->
     <?php
-        if($loggingIn) {
+    // Default to show login form
+    if (!isset($_GET['action']) || $_GET['action'] !== 'register') {
     ?>
-    <div class="formBox">
-        <h3>Login</h3>
-        <form action="/login" method="POST">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-            <button type="submit" class="submitButton">Login</button>
-        </form>
-        <p>Don't have an account? <a href="?action=register">Register here</a></p>
-    </div>
+        <!-- Login Form -->
+		<div class="formBox">
+			<h3>Login</h3>
+			<form action="/login" method="POST">
+				<label for="email">Email:</label>
+				<input type="email" id="email" name="email" required>
+				<label for="password">Password:</label>
+				<input type="password" id="password" name="password" required>
+				<button type="submit" class="submitButton">Login</button>
+			</form>
+			<p class="swapText">Don't have an account? <a href="?action=register">Register here</a></p>
+		</div>
     <?php
-        } else {
+    } else {
     ?>
-    <!-- Registration Form -->
-    <div class="formBox">
-        <h3>Register</h3>
-        <form action="/register" method="POST">
-            <label for="registerName">Full Name:</label>
-            <input type="text" id="registerName" name="registerName" required>
-            <label for="registerEmail">Email:</label>
-            <input type="email" id="registerEmail" name="registerEmail" required>
-            <label for="registerPassword">Password:</label>
-            <input type="password" id="registerPassword" name="registerPassword" required>
-            <label for="registerConfirmPassword">Confirm Password:</label>
-            <input type="password" id="registerConfirmPassword" name="registerConfirmPassword" required>
-            <button type="submit" class="submitButton">Register</button>
-        </form>
-    </div>
+        <!-- Registration Form -->
+		<div class="formBox">
+			<h3>Register</h3>
+			<form action="/register" method="POST">
+				<label for="firstName">Full Name:</label>
+				<input type="text" id="firstName" name="firstName" required>
+				<label for="lastName">Last Name:</label>
+				<input type="text" id="lastName" name="lastName" required>
+				<label for="email">Email:</label>
+				<input type="email" id="email" name="email" required>
+				<label for="password">Password:</label>
+				<input type="password" id="password" name="password" required>
+				<label for="phoneNumber">Phone Number:</label>
+				<input type="text" id="phoneNumber" name="phoneNumber" required>
+				<label for="confirmPassword">Confirm Password:</label>
+				<input type="password" id="confirmPassword" name="confirmPassword" required>
+				<button type="submit" class="submitButton">Register</button>
+			</form>
+			<p class="swapText">Already have an account? <a href="?action=login">Login here</a></p>
+		</div>
     <?php
-        }
+    }
     ?>
 </div>
 
